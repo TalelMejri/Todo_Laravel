ddfe309c719c2a6238c1a34b532b61b347cef8e8
talel <talelmejri8@gmail.com>
2022-12-23 00:36:45 +0100

    champ factory de comment

--- backend/database/factories/CommentFactory.php
+++ backend/database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,10 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "comment"=>$this->faker()->sentence(),
+            "user_id"=>User::get('id')->random(),
+            "post_id"=>Post::get('id')->random(),
+            "created_at"=>now()
         ];
     }
 }
