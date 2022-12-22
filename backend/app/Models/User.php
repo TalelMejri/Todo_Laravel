d599923fb21ad444f18e914d50e510de3da91487
talel <talelmejri8@gmail.com>
2022-12-23 00:16:24 +0100

    relation for user

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts(){
+        return $this->hasMany(posts::class);
+    }
+
+    public function Comments(){
+        return $this->hasMany(Comments::class);
+    }
+
 }
