0f65fea2725befba3da52e380a4ca8ef9db982d1
talel <talelmejri8@gmail.com>
2022-12-23 00:16:17 +0100

    relation for post

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function Categorie(){
+        return $this->belongsTo(Categorie::class);
+    }
 }
