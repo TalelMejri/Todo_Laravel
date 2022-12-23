7efe872e63fd3f7911091bd1f633f7a0689e2d72
talel <talelmejri8@gmail.com>
2022-12-24 00:54:41 +0100

    add route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\PostControler;
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::apiResource('/posts',PostController::class);
