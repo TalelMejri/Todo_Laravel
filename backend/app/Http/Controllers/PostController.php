b6c39751f596c1ff8fc2290d9d9c4dabb7c10413
talel <talelmejri8@gmail.com>
2022-12-27 01:43:20 +0100

    add view

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+       $posts=Post::all();
+       return view('posts.index',compact('posts'));
     }
 
     /**
