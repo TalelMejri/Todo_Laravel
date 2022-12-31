4a4aece0d882f5b87c92aa930378ddfb8cf09ada
talel <talelmejri8@gmail.com>
2022-12-31 02:27:29 +0100

    afficher contenu

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,3 +1,30 @@
-<?php
-  var_dump($posts);
-?>
+<a href="/posts">posts</a>
+<a href="/">home</a>
+
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>category</th>
+            <th>actions</th>
+        </tr>
+
+    </thead>
+    <tbody>
+        @foreach($posts as $post)
+            <tr>
+                <th>{{$post->id}}</th>
+                <td>{{$post->titel}}</td>
+                <td>{{substr($post->body,50).'...'}}</td>
+                <td>{{$post->categorie->name}}</td>
+                <td>
+                    <button>show</button>
+                    <button>edit</button>
+                    <button>delete</button>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
