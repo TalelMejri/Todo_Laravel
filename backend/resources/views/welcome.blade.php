ace64bfa479df59d4c815fe2c9123d5eb1d56f56
talel <talelmejri8@gmail.com>
2022-12-31 02:27:43 +0100

    add navbar

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -20,6 +20,8 @@
             }
         </style>
     </head>
+    <a href="/posts">posts</a>
+<a href="/">home</a>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
