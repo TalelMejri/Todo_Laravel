fe35805cb59ed10afeff5e0f2102b6034cf15549
talel <talelmejri8@gmail.com>
2023-01-01 19:33:50 +0100

    include view navigation in posts

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,5 +1,13 @@
-<a href="/posts">posts</a>
-<a href="/">home</a>
+
+ <!DOCTYPE html>
+ <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+     <head>
+         <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <title>Laravel</title>
+     </head>
+     <body>
+        @include('../navigation')
 
 <table>
     <thead>
@@ -28,3 +36,6 @@
         @endforeach
     </tbody>
 </table>
+
+     </body>
+ </html>
