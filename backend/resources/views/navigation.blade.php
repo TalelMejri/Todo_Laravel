8651394f07a9fee560653670c228cf16fc798840
talel <talelmejri8@gmail.com>
2023-01-01 19:33:16 +0100

    add view navigation

---
+++ backend/resources/views/navigation.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <a href="/posts">posts</a>
+        <a href="/">home</a>
+    </body>
+</html>
