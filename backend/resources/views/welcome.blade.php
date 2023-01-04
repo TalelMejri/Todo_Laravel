10fa489542db3dc693cb7f706d2ab1d07efb8a51
talel <talelmejri8@gmail.com>
2023-01-04 17:12:58 +0100

    clear default

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
             }
         </style>
     </head>
-   @include('../navigation')
+
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
