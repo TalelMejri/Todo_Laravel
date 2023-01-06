59d9bcdf62630d5af0eabf7e9e37770ccf3d33d8
talel <talelmejri8@gmail.com>
2023-01-06 22:16:32 +0100

    link bootstrap

--- backend/resources/views/navigation.blade.php
+++ backend/resources/views/navigation.blade.php
@@ -2,11 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{ mix('js/app.js') }}" defer></script>
+         @vite(['resources/js/app.js'])
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
     </head>
     <body>
-        <a href="/">Home</a>
+        <a class="text-danger btn btn-primary" href="/">Home</a>
         <a href="/posts/">OLD</a>
     </body>
 </html>
