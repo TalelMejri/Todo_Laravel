bc1e69a6e8eab4f6a018a1febcf98b1a8c8bc2b2
talel <talelmejri8@gmail.com>
2023-01-07 14:33:41 +0100

    design page show

--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -8,10 +8,26 @@
     </head>
     <body>
          @include('navigation')
-         <br>
-        {{$post->titel}}
-        {{$post->id}}
-        {{$post->user->email}}
-        {{$post->Categorie->name}}
+     <div class="container">
+        <h5 class="text-center mb-4 text-danger">Posts id : {{$post->id}}</h5>
+        <table class="table table-dark table-striped ">
+            <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Titel</th>
+                  <th scope="col">email user</th>
+                  <th scope="col">Categorie name</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                    <th> {{$post->id}}</th>
+                   <td>{{$post->titel}}</td>
+                   <td> {{$post->user->email}}</td>
+                   <td> {{$post->Categorie->name}}</td>
+                </tr>
+              </tbody>
+        </table>
+    </div>
     </body>
 </html>
