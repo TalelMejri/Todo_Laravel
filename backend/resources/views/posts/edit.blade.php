29eb9ac3eb6d229963485f353940d6f7587540a7
talel <talelmejri8@gmail.com>
2023-01-11 14:49:17 +0100

    fix edit

--- backend/resources/views/posts/edit.blade.php
+++ backend/resources/views/posts/edit.blade.php
@@ -4,7 +4,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('posts.edit') }}
+            {{ __('Edit Post') }}
         </h2>
     </x-slot>
 <div class="container mt-5 py-3 ">
