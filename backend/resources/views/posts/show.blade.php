2e82f0fe299ab8ce4682c2046f5bd788d12786b4
talel <talelmejri8@gmail.com>
2023-01-11 14:49:23 +0100

    fix show

--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('posts.show') }}
+            {{ __('Show Post') }}
         </h2>
     </x-slot>
 <div class="container">
