cc2fec482a61322f6c441f28d0fb2d5ae6f6446e
talel <talelmejri8@gmail.com>
2023-01-12 21:24:19 +0100

    add api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\PostController as ApiPostController;
-use App\Http\Controllers\AppController;
 use App\Http\Controllers\Auth\LoginUser;
-use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/posts', [ApiPostController::class,'index']);
+Route::Post('/logout',[LoginUser::class,'logout']);
 Route::Post('/login',[LoginUser::class,'login']);
-Route::apiResource('/posts',ApiPostController::class);
+
