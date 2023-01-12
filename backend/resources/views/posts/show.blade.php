8795e56231953a28e753609faae8b66dac17ee72
talel <talelmejri8@gmail.com>
2023-01-12 09:42:47 +0100

    src file

--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -8,7 +8,7 @@
             {{ __('Show Post') }}
         </h2>
     </x-slot>
-<div class="container">
+<div class="container mt-5">
     <h5 class="text-center mb-4 text-danger">Posts id : {{$post->id}}</h5>
     <table class="table table-dark table-striped ">
         <thead>
@@ -16,6 +16,7 @@
               <th scope="col">#</th>
               <th scope="col">Titel</th>
               <th scope="col">email user</th>
+               <th >image</th>
               <th scope="col">Categorie name</th>
             </tr>
           </thead>
@@ -24,6 +25,7 @@
                <th> {{$post->id}}</th>
                <td> {{$post->titel}}</td>
                <td> {{$post->user->email}}</td>
+               <td><img src='{{Storage::URL($post->image)}}'></td>
                <td> {{$post->Categorie->name}}</td>
             </tr>
           </tbody>
