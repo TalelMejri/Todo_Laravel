af40161ffa53147b721d6edc2efedccd739e7fab
talel <talelmejri8@gmail.com>
2023-01-12 13:44:32 +0100

    add front with vuetify

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
